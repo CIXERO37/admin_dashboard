259f0f31849fdc186fd2cd1cb0491cd4f3e8f8e4
Fathur Issa Nur Rahman <dlol59005@gmail.com>
2025-12-26 21:46:39 +0700

    fix: whitelist next build folder

---
+++ .next/standalone/.next/server/chunks/ssr/_next-internal_server_app_login_page_actions_d5af5bd5.js
@@ -0,0 +1,3 @@
+module.exports=[8752,a=>{"use strict";var b=a.i(51203);a.s([],4450),a.i(4450),a.s(["40e49854b1a03c097b12330bce056400618a0ab586",()=>b.login],8752)}];
+
+//# sourceMappingURL=_next-internal_server_app_login_page_actions_d5af5bd5.js.map
