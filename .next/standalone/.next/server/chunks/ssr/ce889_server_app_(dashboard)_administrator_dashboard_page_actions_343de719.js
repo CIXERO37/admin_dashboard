f3341620eb7005cdfb79569f84a6e048de53b1e1
Fathur Issa Nur Rahman <dlol59005@gmail.com>
2025-12-26 21:46:39 +0700

    fix: whitelist next build folder

---
+++ .next/standalone/.next/server/chunks/ssr/ce889_server_app_(dashboard)_administrator_dashboard_page_actions_343de719.js
@@ -0,0 +1,3 @@
+module.exports=[58161,a=>{"use strict";var b=a.i(51203);a.s([],51373),a.i(51373),a.s(["0036e64456fb2be9f284aa63a8876a0297f090e2e1",()=>b.logout],58161)}];
+
+//# sourceMappingURL=ce889_server_app_%28dashboard%29_administrator_dashboard_page_actions_343de719.js.map
