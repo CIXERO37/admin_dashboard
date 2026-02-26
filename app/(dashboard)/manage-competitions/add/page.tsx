276bd8f92ea1714405bbc7bc8abd02c43bb6310e
Fathur Issa Nur Rahman <dlol59005@gmail.com>
2026-02-27 05:26:25 +0700

    perubahan sedikit

---
+++ app/(dashboard)/manage-competitions/add/page.tsx
@@ -0,0 +1,9 @@
+import { AddCompetitionForm } from "@/components/competitions/add-competition-form";
+
+export const metadata = {
+  title: "Add Competition - Gameforsmart",
+};
+
+export default function AddCompetitionPage() {
+  return <AddCompetitionForm />;
+}
